648ee42990723c783cfa28120ff556ad1245be12
luantranminh <15520454@gm.uit.edu.vn>
2018-09-28 11:04:50 +0700

    string benchmark

--- string/concatenate_string_test.go
+++ string/concatenate_string_test.go
@@ -1,3 +1,15 @@
+//copy is fastest, ~3x faster than using bytes.Buffer
+/*
+Result:
+1. Copy: 5.6 ns/op
+2. Buffer with Write String: 10.5 ns/op
+3. String Builder: 12.5 ns/op
+4. Buffer with Write Byte: 19.7 ns/op
+5. Concat (+): 34636 ns/op
+*/
+
+// But copy relies on knowing the length of resulting string
+// String builder DIFFERENCES from bytes.Buffer: It's immutable and it can only grow or reset
 package string
 
 import (
@@ -5,6 +17,8 @@ import (
 	"strings"
 	"testing"
 )
+
+var ac []byte = []byte("x")
 
 func BenchmarkConcat(b *testing.B) {
 	var str string
@@ -18,7 +32,19 @@ func BenchmarkConcat(b *testing.B) {
 	}
 }
 
-func BenchmarkBuffer(b *testing.B) {
+func BenchmarkBufferWriteByte(b *testing.B) {
+	var buffer bytes.Buffer
+	for n := 0; n < b.N; n++ {
+		buffer.Write(ac)
+	}
+	b.StopTimer()
+
+	if s := strings.Repeat("x", b.N); buffer.String() != s {
+		b.Errorf("unexpected result; got=%s, want=%s", buffer.String(), s)
+	}
+}
+
+func BenchmarkBufferWriteString(b *testing.B) {
 	var buffer bytes.Buffer
 	for n := 0; n < b.N; n++ {
 		buffer.WriteString("x")
